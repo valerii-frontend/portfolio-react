e01bc9c0274c40022fb005e475da98a8d48c0e02
Valerii from earth <lvaldev.code@gmail.com>
2022-08-07 17:36:59 +0200

    add styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Header.module.css";
 import Topbar from "./Topbar";
+import Tabs from "./Tabs";
 
 export default function Header() {
 	return (
 		<header className={styles.header}>
 			<Topbar />
+			<Tabs />
 		</header>
 	);
 }
--- src/components/Header/Tabs.jsx
+++ src/components/Header/Tabs.jsx
@@ -2,5 +2,22 @@ import React from "react";
 import styles from "./Tabs.module.css";
 
 export default function Tabs() {
-	return <nav className={styles.tabs}></nav>;
+	return (
+		<nav className={styles.tabs}>
+			<ul>
+				<li>
+					<a href='/main'>main.jsx</a>
+				</li>
+				<li>
+					<a href='/works'>works.lib</a>
+				</li>
+				<li>
+					<a href='/about'>about.txt</a>
+				</li>
+				<li>
+					<a href='/contact'>Contact.tel</a>
+				</li>
+			</ul>
+		</nav>
+	);
 }
