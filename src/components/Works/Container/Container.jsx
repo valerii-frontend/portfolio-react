20bebe683e65d145353ea109d282492e424eb1f7
Valerii from earth <lvaldev.code@gmail.com>
2022-08-15 21:18:16 +0200

    remove old components

--- src/components/Works/Container/Container.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./Container.module.css";
-
-export default function Container(props) {
-	return <div className={styles.container}>{props.children}</div>;
-}
