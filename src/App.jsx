949ae756446bd6917cf885580958aac7665f4c0e
Valerii from earth <lvaldev.code@gmail.com>
2022-08-16 22:38:27 +0200

    app

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Taskbar from "./components/Taskbar/Taskbar";
+
+import Contacts from "./pages/Contacts";
+import Works from "./pages/Works";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+function App(props) {
+	return (
+		<BrowserRouter>
+			<div className='App' onContextMenu={(e) => e.preventDefault()}>
+				<Header />
+				<Routes>
+					<Route path='/' element={<Home />} exact />
+					<Route path='/contacts' element={<Contacts />} exact />
+					<Route path='/works' element={<Works />} exact />
+					<Route path='/about' element={<About />} exact />
+				</Routes>
+
+				<Taskbar />
+			</div>
+		</BrowserRouter>
+	);
+}
+
+export default App;
