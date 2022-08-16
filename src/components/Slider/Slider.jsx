f2f5bc007bc10ea8f1cc676e29b1d3432765c2f0
Valerii from earth <lvaldev.code@gmail.com>
2022-08-16 23:08:33 +0200

    add slider content arr

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -6,6 +6,23 @@ import "swiper/css";
 import "swiper/css/autoplay";
 
 export default function Slider({ setSwiperEl }) {
+	const sliderContent = [
+		{
+			name: "HTML",
+			icon: "html",
+			p1: `The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed
+		in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting
+		languages such as JavaScript.`,
+			p2: `Web browsers receive HTML documents from a web server or from local storage and render the documents into
+		multimedia web pages. HTML describes the structure of a web page semantically and originally included cues
+		for the appearance of the document.`,
+			p3: `I have good knowledge of working with HTML, popular attributes, and tags. As well as building clean and
+		semantic code using modern practices`,
+			p4: `Now it is less often necessary to use pure HTML, the layout is increasingly being taken over by libraries
+		and frameworks`,
+		},
+	];
+
 	return (
 		<div className={styles.slider}>
 			<Swiper
@@ -17,9 +34,15 @@ export default function Slider({ setSwiperEl }) {
 				autoplay
 				speed={500}
 				loop>
-				<SwiperSlide className={styles.slide} data-swiper-autoplay='5000'>
-					<p>HTML</p>
-				</SwiperSlide>
+				{sliderContent.map((slide) => (
+					<SwiperSlide className={styles.slide} data-swiper-autoplay='5000'>
+						<h2 className={styles[`${slide.icon}`]}>{slide.name}</h2>
+						<p>{slide.p1}</p>
+						<p>{slide.p2}</p>
+						<p className={styles.me}>{slide.p3}</p>
+						<p className={styles.me}>{slide.p4}</p>
+					</SwiperSlide>
+				))}
 				<SwiperSlide className={styles.slide} data-swiper-autoplay='5000'>
 					<p>VSCODE</p>
 				</SwiperSlide>
