65f9ac7fe15831e844eaf2791c3ac5b4c1d2919b
Valerii Lozghachov <v.lozghachov@rolladengroup.de>
2022-08-26 15:48:43 +0200

    add svg mask

--- src/components/Header/ControlsBar.jsx
+++ src/components/Header/ControlsBar.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import styles from "./ControlBar.module.css";
-import newFile from "../../assets/icons/new-file.svg";
-import newFolder from "../../assets/icons/new-folder.svg";
-import refresh from "../../assets/icons/refresh.svg";
-import collapse from "../../assets/icons/collapse-all.svg";
-import moreIcon from "../../assets/icons/more.svg";
 
 export default function ControlBar() {
 	return (
 		<div className={styles.filesControl}>
-			<span>explorer: portfolio: </span>
+			<strong>explorer: portfolio: </strong>
 			<div>
-				<img src={newFile} alt='new-file' />
-				<img src={newFolder} alt='new folder' />
-				<img src={refresh} alt='refresh' />
-				<img src={collapse} alt='collapse' />
-				<img src={moreIcon} alt='more' />
+				<span className={styles.newFile} alt='new-file' />
+				<span className={styles.newFolder} alt='new folder' />
+				<span className={styles.refresh} alt='refresh' />
+				<span className={styles.collapse} alt='collapse' />
+				<span className={styles.more} alt='more' />
 			</div>
 		</div>
 	);
