29058c5a3cd079d1f681236144ebc4152a0f898f
Valerii from earth <lvaldev.code@gmail.com>
2022-09-03 18:40:22 +0200

    rebuild titles anim

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<div className='titlesWrap'>
-			<div className='titles'>
-				<h1>Valerii Lozghachov</h1>
-				<h2>Frontend Developer</h2>
-			</div>
-		</div>
 		<App />
 	</React.StrictMode>
 );
