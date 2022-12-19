cf2e90600d0a7c618c9e2e88cb06214d39604432
valerii <lvaldev.code@gmail.com>
2022-12-19 22:32:46 +0100

    add lazy to model and slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import styles from "./styles/Home.module.css";
-import Model from "../components/Model/Model";
-import Slider from "../components/Slider/Slider";
+
 import Sidebar from "../components/Sidebar/Sidebar";
 import HomeSidebar from "../components/Sidebar/SidebarHomeContent";
-import Readme from "../components/Readme/Readme";
+
+const Readme = lazy(() => import("../components/Readme/Readme"));
+const Model = lazy(() => import("../components/Model/Model"));
+const Slider = lazy(() => import("../components/Slider/Slider"));
 
 export default function Main() {
-	const [swiperEl, setSwiperEl] = useState(1);
-	const [isReadme, setIsReadme] = useState(false);
-	const slideTo = (index) => swiperEl.slideTo(index, 500);
+  const [swiperEl, setSwiperEl] = useState(1);
+  const [isReadme, setIsReadme] = useState(false);
+  const slideTo = (index) => swiperEl.slideTo(index, 500);
 
-	return (
-		<main className={styles.main}>
-			<Sidebar>
-				<HomeSidebar slideTo={slideTo} setIsReadme={setIsReadme} isReadme={isReadme} />
-			</Sidebar>
-			<div className={styles.wrapper}>
-				{isReadme && <Readme setIsReadme={setIsReadme} />}
-				<Model slideTo={slideTo} />
-				<Slider setSwiperEl={setSwiperEl} />
-			</div>
-		</main>
-	);
+  return (
+    <main className={styles.main}>
+      <Sidebar>
+        <HomeSidebar slideTo={slideTo} setIsReadme={setIsReadme} isReadme={isReadme} />
+      </Sidebar>
+      <div className={styles.wrapper}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Model slideTo={slideTo} />
+          {isReadme && <Readme setIsReadme={setIsReadme} />}
+          <Slider setSwiperEl={setSwiperEl} />
+        </Suspense>
+      </div>
+    </main>
+  );
 }
